4a824b95a36611aab5ff29c156de82cbef1ff900
notabelincoln <abejordandx@gmail.com>
2021-02-19 01:18:43 -0500

    Updated spacing

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 
 int main(int argc, char** argv)
 {
-	printf("%-24s %-24s %-20s\n","Type","Decimal","Hex");
-	printf("%-24s %-24lu\n","Char Size",sizeof(char));
-	printf("%-24s %-24lu\n","Short Size",sizeof(short));
-	printf("%-24s %-24lu\n","Int Size",sizeof(int));
-	printf("%-24s %-24lu\n","Long Size",sizeof(long));
-	printf("%-24s %-24lu\n","Float Size",sizeof(float));
-	printf("%-24s %-24lu\n","Double Size",sizeof(double));
-	printf("%-24s %-24d 0x%-24X\n","INT_MAX",INT_MAX,INT_MAX);
-	printf("%-24s %-24u 0x%-24X\n","UINT_MAX",UINT_MAX,UINT_MAX);
-	printf("%-24s %-24ld 0x%-24X\n","LONG_MAX",LONG_MAX,LONG_MAX);
-	printf("%-24s %-24lu 0x%-24X\n","ULONG_MAX",ULONG_MAX,ULONG_MAX);
+	printf("%-16s %-16s %-16s\n","Type","Decimal","Hex");
+	printf("%-16s %-16lu\n","Char Size",sizeof(char));
+	printf("%-16s %-16lu\n","Short Size",sizeof(short));
+	printf("%-16s %-16lu\n","Int Size",sizeof(int));
+	printf("%-16s %-16lu\n","Long Size",sizeof(long));
+	printf("%-16s %-16lu\n","Float Size",sizeof(float));
+	printf("%-16s %-16lu\n","Double Size",sizeof(double));
+	printf("%-16s %-16d 0x%-16X\n","INT_MAX",INT_MAX,INT_MAX);
+	printf("%-16s %-16u 0x%-16X\n","UINT_MAX",UINT_MAX,UINT_MAX);
+	printf("%-16s %-16ld 0x%-16lX\n","LONG_MAX",LONG_MAX,LONG_MAX);
+	printf("%-16s %-16lu 0x%-16lX\n","ULONG_MAX",ULONG_MAX,ULONG_MAX);
 	return 0;
 }
